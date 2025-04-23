1fbea109631e9988de0f473f126942c45229d029
matiasvlevi <matiasvlevi@gmail.com>
2025-04-23 11:47:33 -0400

    use arrowfunctions

--- test.ts
+++ test.ts
@@ -1,5 +1,5 @@
 // Test forward/backward motion
-input.onButtonPressed(Button.A, function () {
+input.onButtonPressed(Button.A, () => {
     basic.showString("FB");
     ada.forward(35, 0.5);
     ada.backwards(35, 0.5);
@@ -7,7 +7,7 @@ input.onButtonPressed(Button.A, function () {
 });
 
 // Test left/right motion
-input.onButtonPressed(Button.B, function () {
+input.onButtonPressed(Button.B, () => {
     basic.showString("LR");
     ada.turnLeft(50, 0.5);
     ada.turnRight(50, 0.5);
